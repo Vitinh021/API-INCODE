af975232746b29a2b0bfca71276eeb2ce209d3f6
Jose Estevao <jaae@discente.ifpe.edu.br>
2025-06-25 19:27:06 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const routes = require('./routes');
 
+app.use(cors()); 
 app.use(express.json());
 
 app.use('/api', routes);
